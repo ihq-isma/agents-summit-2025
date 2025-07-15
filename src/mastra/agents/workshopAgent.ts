6869403909f69209b128a785bf281ca0a0ddc151
Isma Perez <ismael.perez@intenthq.com>
2025-07-15 16:27:14 +0100

    adding builtin evals to workshop agent

--- src/mastra/agents/workshopAgent.ts
+++ src/mastra/agents/workshopAgent.ts
@@ -3,18 +3,29 @@ import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
 import { LibSQLStore } from '@mastra/libsql';
 import { selectAll } from '../tools/databaseTools';
+import { SummarizationMetric } from "@mastra/evals/llm";
+import {
+  ContentSimilarityMetric,
+  ToneConsistencyMetric,
+} from '@mastra/evals/nlp';
 
+const model = openai('gpt-4o-mini');
 export const workshopAgent = new Agent({
   name: 'Workshop Agent',
   instructions: `
       You are a helpful agent with knowledge about dashboards, clients and users in a database.
       You are being used as an example in an AI agent development workshop, keep this in mind when providing answers and feel free to include explanations about your reasoning in your answers.
 `,
-  model: openai('gpt-4o-mini'),
+  model, 
   tools: { selectAll },
   memory: new Memory({
     storage: new LibSQLStore({
       url: ':memory:', // path is relative to the .mastra/output directory
     }),
   }),
+  evals: {
+    summarization: new SummarizationMetric(model),
+    contentSimilarity: new ContentSimilarityMetric(),
+    tone: new ToneConsistencyMetric(),
+  },
 });
